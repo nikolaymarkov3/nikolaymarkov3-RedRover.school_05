e64082db4e3f2bde0c4f38c90d776b0e1bc3df7f
Natalya Yanochkova <81821452+NatalyaYanochkova@users.noreply.github.com>
2022-11-12 22:48:26 +0200

    testSearchLanguageFieldByName_HappyPath() added (#22)

---
+++ src/test/java/NatalyaYanochkovaTest.java
@@ -0,0 +1,38 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+import java.util.Collections;
+import java.util.List;
+
+public class NatalyaYanochkovaTest extends BaseTest{
+    @Test
+    public void testSearchLanguageFieldByName_HappyPath(){
+        final String BASE_URL = "https://www.99-bottles-of-beer.net/";
+        final String Language_Python = "python";
+
+        getDriver().get(BASE_URL);
+        WebElement searchLanguagesMenu = getDriver().findElement(
+                By.xpath("//*[@id=\"menu\"]/li[3]/a")
+        );
+        searchLanguagesMenu.click();
+
+        WebElement searchForField = getDriver().findElement(By.name("search"));
+        searchForField.click();
+        searchForField.sendKeys(Language_Python);
+
+        WebElement goButton = getDriver().findElement(By.name("submitsearch"));
+        goButton.click();
+
+        List<WebElement> languagesNameList = Collections.singletonList(getDriver().findElement(
+                By.xpath("//table[@id='category']/tbody/tr/td[1]/a")));
+
+        Assert.assertTrue(languagesNameList.size()>0);
+
+        for(int i=0; i<languagesNameList.size(); i++){
+            Assert.assertTrue(languagesNameList.get(i).getText().toLowerCase().contains(Language_Python));
+        }
+    }
+}
