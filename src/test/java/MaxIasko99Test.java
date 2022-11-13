7495e0956f42b8792b9c14343136c736fdf739ca
maksymiasko <maksymiasko@gmail.com>
2022-11-13 20:50:26 +0000

    testSearchForLanguage_HappyPath() added

---
+++ src/test/java/MaxIasko99Test.java
@@ -0,0 +1,38 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+import java.util.List;
+
+public class MaxIasko99Test extends BaseTest {
+
+    @Test
+    public void testSearchForLanguage_HappyPath() {
+        final String BASE_URL = "https://www.99-bottles-of-beer.net/";
+        final String LANGUAGE_NAME = "python";
+
+        getDriver().get(BASE_URL);
+
+        WebElement searchLanguagesMenu = getDriver().findElement(
+                By.xpath("//ul[@id = 'menu']/li/a[@href = '/search.html']"));
+        searchLanguagesMenu.click();
+
+        WebElement searchForField = getDriver().findElement(By.name("search"));
+        searchForField.click();
+        searchForField.sendKeys(LANGUAGE_NAME);
+
+        WebElement goButton = getDriver().findElement(By.name("submitsearch"));
+        goButton.click();
+
+        List<WebElement> languagesNamesList = getDriver().findElements(
+                By.xpath("//table[@id='category']/tbody/tr/td[1]/a"));
+
+        Assert.assertTrue(languagesNamesList.size() > 0);
+
+        for (WebElement webElement : languagesNamesList) {
+            Assert.assertTrue(webElement.getText().toLowerCase().contains(LANGUAGE_NAME));
+        }
+    }
+}
