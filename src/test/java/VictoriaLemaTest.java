929d10f7bc6091ad870f1e88a74d6d9561522120
VictoriaLema <115832814+VictoriaLema@users.noreply.github.com>
2022-11-14 18:53:56 +0300

    testSearchForLanguageByName_HappyPath() added (#37)

---
+++ src/test/java/VictoriaLemaTest.java
@@ -0,0 +1,40 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+import java.util.List;
+
+public class VictoriaLemaTest extends BaseTest {
+
+    static final String BASE_URL = "https://www.99-bottles-of-beer.net/";
+    @Test
+    public void testSearchForLanguageByName_HappyPath() {
+        final String LANGUAGE_NAME = "python";
+
+        getDriver().get(BASE_URL);
+
+        WebElement searchLanguagesMenu = getDriver().findElement(
+                By.xpath("//ul[@id='menu']/li/a[@href = '/search.html']")
+        );
+        searchLanguagesMenu.click();
+
+        WebElement searchForField = getDriver().findElement(By.name("search"));
+        searchForField.click();
+        searchForField.sendKeys(LANGUAGE_NAME);
+
+        WebElement goButton = getDriver().findElement(By.name("submitsearch"));
+        goButton.click();
+
+        List<WebElement> languagesNameList = getDriver().findElements(
+                By.xpath("//table[@id='category']/tbody/tr/td[1]/a")
+        );
+
+        Assert.assertTrue(languagesNameList.size() > 0);
+
+        for (int i = 0; i < languagesNameList.size(); i++){
+            Assert.assertTrue(languagesNameList.get(i).getText().toLowerCase().contains(LANGUAGE_NAME));
+        }
+    }
+}
