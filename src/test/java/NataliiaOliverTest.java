491f2741abc0dfba614c868f515534422fd36347
NataliiaOliver <103964206+NataliiaOliver@users.noreply.github.com>
2022-11-15 21:46:54 -0600

    ATStartNavigationSubmenuTeam_testTeamLinks()_NataliiaOliver created, methods getTeamLinks() and getAllCreatorsLinks() created (#67)

--- src/test/java/NataliiaOliverTest.java
+++ src/test/java/NataliiaOliverTest.java
@@ -10,15 +10,31 @@ import java.util.List;
 
 public class NataliiaOliverTest extends BaseTest {
 
-    static final String BASE_URL = "https://www.99-bottles-of-beer.net/";
+    private final String BASE_URL = "https://www.99-bottles-of-beer.net/";
+    private final By START_MENU = By.xpath("//div[@id='navigation']//a[@href='/']");
+    private final By SUBMENU_TEAM = By.xpath("//div[@id='navigation']//a[@href='team.html']");
 
-    public static List<String> WebElementToString(List<WebElement> elementList) {
+    public List<String> WebElementToString(List<WebElement> elementList) {
         List<String> stringList = new ArrayList<>();
         for (WebElement element : elementList) {
             stringList.add(element.getText());
         }
 
         return stringList;
+    }
+
+    public List<WebElement> getTeamLinks() {
+
+        return getDriver().findElements(By.xpath("//div[@id='main']/p/a"));
+    }
+
+    public List<String> getAllCreatorsLinks() {
+        List<String> linksText = new ArrayList<>();
+        for (WebElement element: getTeamLinks()) {
+            linksText.add(element.getText());
+        }
+
+        return linksText;
     }
 
     @Test
@@ -66,5 +82,25 @@ public class NataliiaOliverTest extends BaseTest {
         List<String> actualTeamNames = WebElementToString(creatorsNames);
 
         Assert.assertEquals(actualTeamNames, expectedTeamNames);
+    }
+
+    @Test
+    public void testTeamLinks() {
+        List<String> expectedCreatorsAllLinks = new ArrayList<>();
+        expectedCreatorsAllLinks.add("ls-la.net");
+        expectedCreatorsAllLinks.add("E-Taste.org");
+        expectedCreatorsAllLinks.add("sts.synflood.de");
+
+        getDriver().get(BASE_URL);
+
+        WebElement startMenu = getDriver().findElement(START_MENU);
+        startMenu.click();
+
+        WebElement teamSubmenu = getDriver().findElement(SUBMENU_TEAM);
+        teamSubmenu.click();
+
+        List<String> actualCreatorsAllLinks = getAllCreatorsLinks();
+
+        Assert.assertEquals(actualCreatorsAllLinks, expectedCreatorsAllLinks);
     }
 }
