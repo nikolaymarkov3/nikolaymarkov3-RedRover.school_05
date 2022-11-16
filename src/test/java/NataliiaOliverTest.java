b2557c01d7a7d77b3e220de86047f8d803d081ab
NataliiaOliver <103964206+NataliiaOliver@users.noreply.github.com>
2022-11-15 18:06:44 -0600

    created testTeamNames(), added method List<String> WebElementToString(List<WebElement> elementList) (#58)

--- src/test/java/NataliiaOliverTest.java
+++ src/test/java/NataliiaOliverTest.java
@@ -4,11 +4,22 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class NataliiaOliverTest extends BaseTest {
 
     static final String BASE_URL = "https://www.99-bottles-of-beer.net/";
+
+    public static List<String> WebElementToString(List<WebElement> elementList) {
+        List<String> stringList = new ArrayList<>();
+        for (WebElement element : elementList) {
+            stringList.add(element.getText());
+        }
+
+        return stringList;
+    }
 
     @Test
     public void testSearchForLanguageByName_HappyPath() {
@@ -35,5 +46,25 @@ public class NataliiaOliverTest extends BaseTest {
         for (int i = 0; i < languageNamesList.size(); i++) {
             Assert.assertTrue(languageNamesList.get(i).getText().toLowerCase().contains(LANGUAGE_NAME));
         }
+    }
+
+    @Test
+    public void testTeamNames() {
+        List<String> expectedTeamNames = new ArrayList<>(
+                Arrays.asList("Oliver Schade", "Gregor Scheithauer", "Stefan Scheler"));
+
+        getDriver().get(BASE_URL);
+
+        WebElement startMenu = getDriver().findElement(By.xpath("//div[@id='navigation']//a[@href='/']"));
+        startMenu.click();
+
+        WebElement teamSubmenu = getDriver().findElement(By.xpath("//div[@id='navigation']//a[@href='team.html']"));
+        teamSubmenu.click();
+
+        List<WebElement> creatorsNames = getDriver().findElements(
+                By.xpath("//div[@id='main']/h3"));
+        List<String> actualTeamNames = WebElementToString(creatorsNames);
+
+        Assert.assertEquals(actualTeamNames, expectedTeamNames);
     }
 }
