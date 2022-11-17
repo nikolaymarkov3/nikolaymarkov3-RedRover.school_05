c6aad5407defb830559ebbfcdbc96c66871d5333
MariyaSvetlova <99127640+lmt3103@users.noreply.github.com>
2022-11-17 10:35:08 +0500

    [AT][Main][header][h1] Verify header on start page <MariyaSvetlova> (#83)
    * ATMainHeaderH1_testVerifyHeaderOnStartPage_MariyaSvetlova

---
+++ src/test/java/MariyaSvetlovaTest.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+public class MariyaSvetlovaTest extends BaseTest {
+
+    static final String BASE_URL = "https://www.99-bottles-of-beer.net/";
+
+    @Test
+    public void testVerifyHeaderH1OnStartPage() {
+        final String HEADER_H1 = "99 Bottles of Beer";
+
+        getDriver().get(BASE_URL);
+
+        WebElement headerH1 = getDriver().findElement(
+                By.xpath("//div[@id='header']/h1")
+        );
+
+        Assert.assertEquals(headerH1.getText(),HEADER_H1);
+    }
+}
