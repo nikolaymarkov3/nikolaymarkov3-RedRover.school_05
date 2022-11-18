4a189455cb7eba563c634ddc0ee4fe502def82d4
Aly <65147059+Albina-star@users.noreply.github.com>
2022-11-18 18:43:30 -0500

    testVerifyTextOfFooterMenuSearchLanguages_HappyPath() added (#103)

--- src/test/java/Albina_starTest.java
+++ src/test/java/Albina_starTest.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class Albina_starTest extends BaseTest {
 
     @Test
-    public void testSearchForLanguageByName_HappyPath(){
+    public void testSearchForLanguageByName_HappyPath() {
         final String BASE_URL = "https://99-bottles-of-beer.net/";
         final String LANGUAGE_PYTHON = "python";
 
@@ -34,5 +34,31 @@ public class Albina_starTest extends BaseTest {
         for (int i = 0; i < languagesNamesList.size(); i++) {
             Assert.assertTrue(languagesNamesList.get(i).getText().toLowerCase().contains(LANGUAGE_PYTHON));
         }
+    }
+
+    @Test
+    public void testVerifyTextOfFooterMenuSearchLanguages_HappyPath() {
+        final String BASE_URL = "https://99-bottles-of-beer.net/";
+        final String SEARCH_LANGUAGES = "Search Languages";
+
+        getDriver().get(BASE_URL);
+
+        WebElement searchLanguagesFooterMenu = getDriver().findElement(
+                By.xpath("//div[@id=\"footer\"]/p/a[3]"));
+        searchLanguagesFooterMenu.click();
+
+        WebElement searchLanguages = getDriver().findElement(
+                By.xpath("//div[@id=\"main\"]/h2"));
+        searchLanguages.getText();
+
+        Assert.assertEquals(searchLanguages.getText(), SEARCH_LANGUAGES);
+
+        WebElement searchFor = getDriver().findElement(
+                By.xpath("//div/div[3]/form/p/input[1]"));
+        searchFor.isDisplayed();
+
+        Assert.assertTrue(searchFor.isDisplayed());
+
+
     }
 }
