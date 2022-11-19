23794f585d1b693e377dc22768699ed2b5c86052
MariiaBatozskaia <75219033+mbatoz@users.noreply.github.com>
2022-11-19 18:50:12 +0400

    set static final String BASE_URL (#31)
    verifyWishesFromTheTeamTest added
    
    Co-authored-by: mbatoz <mbtozskaya@yandex.ru>

--- src/test/java/mbatozTest.java
+++ src/test/java/mbatozTest.java
@@ -4,14 +4,14 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
-import java.io.ByteArrayOutputStream;
 import java.util.List;
 
 public class mbatozTest extends BaseTest {
+    static final String BASE_URL = "https://www.99-bottles-of-beer.net";
 
     @Test
     public void testSearchForLanguageByName_HappyPath() {
-        final String BASE_URL = "https://www.99-bottles-of-beer.net";
+
         final String LANGUAGE_PYTHON = "python";
 
         getDriver().get(BASE_URL);
@@ -36,5 +36,21 @@ public class mbatozTest extends BaseTest {
         for (int i = 0; i < languageNamesList.size(); i ++) {
             Assert.assertTrue(languageNamesList.get(i).getText().toLowerCase().contains(LANGUAGE_PYTHON));
         }
+    }
+
+
+    @Test
+    public void testStartForVerifyWishesFromTheTeam_HappyPath() {
+        getDriver().get(BASE_URL);
+
+        String ExpectedResult = "This Website holds a collection of the Song 99 Bottles of Beer programmed in different programming languages. " +
+                "Actually the song is represented in 1500 different programming languages and variations. " +
+                "For more detailed information refer to historic information.";
+        String ActualResult = getDriver().findElement(
+                By.xpath("//p[contains(text(),'This Website holds a collection of the Song')]")
+        ).getText();
+
+        Assert.assertEquals(ActualResult, ExpectedResult);
+
     }
 }
