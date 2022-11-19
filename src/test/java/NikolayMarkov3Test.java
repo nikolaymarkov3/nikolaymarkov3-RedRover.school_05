a609e46e6c05bf824d3c94e0423632ac5147c2be
Марков Николай Николаевич <103429303+nikolaymarkov3@users.noreply.github.com>
2022-11-19 19:49:24 +0700

    testSearchSearchForLanguagesByNameJava_HappyPath() added (#132)

--- src/test/java/NikolayMarkov3Test.java
+++ src/test/java/NikolayMarkov3Test.java
@@ -59,4 +59,33 @@ public class NikolayMarkov3Test extends BaseTest {
 
         Assert.assertTrue(languagesNamesList.size() == 0);
     }
+
+    @Test
+    public void testMenuSearchSearchForLanguagesByNameJava_HappyPath() {
+        final String BASE_URL = "https://www.99-bottles-of-beer.net/";
+        final String LANGUAGE_NAME = "java";
+
+        getDriver().get(BASE_URL);
+        WebElement searchLanguagesMenu = getDriver().findElement(
+                By.xpath("//ul[@id = 'menu']/li/a[@href = '/search.html']")
+        );
+        searchLanguagesMenu.click();
+
+        WebElement searchForFiend = getDriver().findElement(By.name("search"));
+        searchForFiend.click();
+        searchForFiend.sendKeys(LANGUAGE_NAME);
+
+        WebElement goButton = getDriver().findElement(By.name("submitsearch"));
+        goButton.click();
+
+        List<WebElement> languagesNamesList = getDriver().findElements(
+                By.xpath("//table[@id = 'category']/tbody/tr/td[1]/a")
+        );
+
+        Assert.assertTrue(languagesNamesList.size() > 0);
+
+        for (int i = 0; i < languagesNamesList.size(); i ++){
+            Assert.assertTrue(languagesNamesList.get(i).getText().toLowerCase().contains(LANGUAGE_NAME));
+        }
+    }
 }
