47a0795c78851175e94fcdaf0762ddfefbe634ee
Tatyana-Belikova <117529428+Tatyana-Belikova@users.noreply.github.com>
2022-11-19 15:54:36 +0200

    StartNavigationSubmenuImpressumVerifyInfoLabelText (#131)

--- src/test/java/TatyanaBelikovaTest.java
+++ src/test/java/TatyanaBelikovaTest.java
@@ -6,6 +6,9 @@ import runner.BaseTest;
 import java.util.List;
 
 public class TatyanaBelikovaTest extends BaseTest {
+
+    final String BASE_URL = "https://www.99-bottles-of-beer.net/";
+    String SUB_MENU_OPTION = "";
     @Test
     public void testSearchForLanguageFieldByName_HappyPath() {
         final String BASE_URL = "https://www.99-bottles-of-beer.net/";
@@ -28,8 +31,21 @@ public class TatyanaBelikovaTest extends BaseTest {
         List<WebElement> languageNameList = getDriver().findElements(By.xpath("//table[@id='category']/tbody/tr/td[1]/a"));
 
         Assert.assertTrue(languageNameList.size() > 0);
-         for (int i = 0; i < languageNameList.size(); i++){
-             Assert.assertTrue(languageNameList.get(i).getText().toLowerCase().contains(LANGUAGE_NAME));
+        for (int i = 0; i < languageNameList.size(); i++){
+            Assert.assertTrue(languageNameList.get(i).getText().toLowerCase().contains(LANGUAGE_NAME));
+        }
+    }
+    @Test
+    public void testSubmenuImpressumLabelText_Privacy() {
+
+        SUB_MENU_OPTION = "impressum.html";
+        final String expectedResult = "Privacy";
+
+        getDriver().get(BASE_URL);
+
+        WebElement subMenuImpressum = getDriver().findElement(
+                By.xpath("//div[@id='navigation']/ul//li/a[@href='" +SUB_MENU_OPTION+ "']"));
+
+        Assert.assertEquals(subMenuImpressum.getText(), expectedResult);
          }
     }
-}
