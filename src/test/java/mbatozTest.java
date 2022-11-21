7f036d8a6d843a86207559316c410eaa1b1aa4ee
Mariia Batozskaia <75219033+mbatoz@users.noreply.github.com>
2022-11-21 22:48:51 +0400

    refactor testSearchForLanguageByName (#172)

--- src/test/java/mbatozTest.java
+++ src/test/java/mbatozTest.java
@@ -1,47 +1,85 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
+import java.util.ArrayList;
 import java.util.List;
 
+
+
 public class mbatozTest extends BaseTest {
-    static final String BASE_URL = "https://www.99-bottles-of-beer.net";
+        final String BASE_URL = "https://www.99-bottles-of-beer.net/";
 
-    @Test
-    public void testSearchForLanguageByName_HappyPath() {
+        final static By SEARCH_LANGUAGES_MENU =  By.xpath("//ul[@id = 'menu']/li/a[@href = '/search.html']");
+        final static By SEARCH_FOR_FIELD = By.name("search");
+        final static By GO_BUTTON = By.name("submitsearch");
+        final static By LANGUAGES_NAMES_LIST = By.xpath("//table[@id='category']/tbody/tr/td[1]/a");
 
-        final String LANGUAGE_PYTHON = "python";
-
-        getDriver().get(BASE_URL);
-        WebElement searchLanguagesMenu = getDriver().findElement(
-                By.xpath("//ul[@id='menu']/li//a[@href='/search.html']")
-        );
-        searchLanguagesMenu.click();
-
-        WebElement searchforField = getDriver().findElement(By.name("search"));
-        searchforField.click();
-        searchforField.sendKeys(LANGUAGE_PYTHON);
-        //searchforField.clear();
-
-        WebElement goButton = getDriver().findElement(By.name("submitsearch"));
-        goButton.click();
-
-        List<WebElement> languageNamesList = getDriver().findElements(
-                By.xpath("//table[@id='category']/tbody/tr/td[1]/a"));
-
-        Assert.assertTrue(languageNamesList.size() > 0);
-
-        for (int i = 0; i < languageNamesList.size(); i ++) {
-            Assert.assertTrue(languageNamesList.get(i).getText().toLowerCase().contains(LANGUAGE_PYTHON));
+        private void openBaseURL(WebDriver driver) {
+            driver.get(BASE_URL);
         }
-    }
 
+        private WebElement getElement(By by, WebDriver driver) {
+
+            return driver.findElement(by);
+        }
+
+        private List<WebElement> getListOfElements(By by, WebDriver driver) {
+
+            return driver.findElements(by);
+        }
+
+        private void click(By by, WebDriver driver) {
+            getElement(by, driver).click();
+        }
+
+        private void input(String text, By by, WebDriver driver) {
+            getElement(by, driver).sendKeys(text);
+        }
+
+        private int getListSize(By by, WebDriver driver) {
+
+            return getListOfElements(by, driver).size();
+        }
+
+        private List<String> getElementsText(By by, WebDriver driver) {
+            List<WebElement> elementsList = getListOfElements(by, driver);
+            List<String> textList = new ArrayList<>();
+
+            for (WebElement element : elementsList) {
+                textList.add(element.getText().toLowerCase());
+            }
+
+            return textList;
+        }
+
+        @Test
+        public void testSearchForLanguageByName_HappyPath() {
+            final String LANGUAGE_NAME = "python";
+
+            openBaseURL(getDriver());
+            click(SEARCH_LANGUAGES_MENU, getDriver());
+            click(SEARCH_FOR_FIELD, getDriver());
+            input(LANGUAGE_NAME, SEARCH_FOR_FIELD, getDriver());
+            click(GO_BUTTON, getDriver());
+
+            List<String> languageNames = getElementsText(LANGUAGES_NAMES_LIST, getDriver());
+
+            Assert.assertTrue(languageNames.size() > 0);
+
+            for (String languageName : languageNames) {
+                Assert.assertTrue(languageName.contains(LANGUAGE_NAME));
+            }
+        }
 
     @Test
     public void testStartForVerifyWishesFromTheTeam_HappyPath() {
-        getDriver().get(BASE_URL);
+
+        openBaseURL(getDriver());
+
 
         String ExpectedResult = "This Website holds a collection of the Song 99 Bottles of Beer programmed in different programming languages. " +
                 "Actually the song is represented in 1500 different programming languages and variations. " +
