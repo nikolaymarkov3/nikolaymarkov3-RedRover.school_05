e044b250166189c5d50478cafcc1300795bb6c94
Юлия Л <113936533+Yuliya-14@users.noreply.github.com>
2022-11-21 21:44:25 +0300

    testSearchForLanguageByName_HappyPath() refactored, locators and methods added (#171)

--- src/test/java/Yuliya14Test.java
+++ src/test/java/Yuliya14Test.java
@@ -1,46 +1,82 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import static java.sql.DriverManager.getDriver;
+
 public class Yuliya14Test extends BaseTest {
+    final String BASE_URL = "https://www.99-bottles-of-beer.net/";
+
+    final static By SEARCH_LANGUAGES_MENU = By.xpath("//ul[@id='menu']/li/a[@href = '/search.html']");
+    final static By SEARCH_FOR_FIELD = By.name("search");
+    final static By GO_BUTTON = By.name("submitsearch");
+    final static By LANGUAGES_NAMES_LIST = By.xpath("//table[@id='category']/tbody/tr/td[1]/a");
+
+    private void openBaseURL(WebDriver driver) {
+        driver.get(BASE_URL);
+    }
+
+    private WebElement getElement(By by, WebDriver driver) {
+
+        return  driver.findElement(by);
+    }
+
+    private List<WebElement> getListofElements(By by, WebDriver driver) {
+
+        return  driver.findElements(by);
+    }
+
+    public void click(By by, WebDriver driver){
+        getElement(by, driver).click();
+    }
+
+    private void input(String text, By by, WebDriver driver) {
+        getElement(by, driver).sendKeys(text);
+    }
+
+    private int getListSize(By by, WebDriver driver) {
+
+        return getListofElements(by, driver).size();
+    }
+
+    private List<String> getElementsText(By by, WebDriver driver) {
+        List <WebElement> elementsList = getListofElements(by, driver);
+        List<String> textList = new ArrayList<>();
+
+        for (WebElement element : elementsList) {
+            textList.add(element.getText().toLowerCase());
+        }
+
+        return textList;
+    }
 
     @Test
     public void testSearchForLanguageByName_HappyPath() {
-        final String BASE_URL = "https://www.99-bottles-of-beer.net/";
         final String LANGUAGE_NAME = "python";
 
-        getDriver().get(BASE_URL);
+        openBaseURL(getDriver());
+        click(SEARCH_LANGUAGES_MENU , getDriver());
+        click(SEARCH_FOR_FIELD, getDriver());
+        input(LANGUAGE_NAME, SEARCH_FOR_FIELD, getDriver());
+        click(GO_BUTTON, getDriver());
 
-        WebElement searchLanguagesMenu = getDriver().findElement(
-                By.xpath("//ul[@id='menu']/li/a[@href = '/search.html']")
-        );
-        searchLanguagesMenu.click();
+        List<String> languageNames = getElementsText(LANGUAGES_NAMES_LIST, getDriver());
 
-        WebElement searchForField = getDriver().findElement(By.name("search"));
-        searchForField.click();
-        searchForField.sendKeys(LANGUAGE_NAME);
+        Assert.assertTrue(languageNames.size() > 0);
 
-        WebElement goButton = getDriver().findElement(By.name("submitsearch"));
-        goButton.click();
-
-        List<WebElement> languagesNamesList = getDriver().findElements(
-                By.xpath("//table[@id='category']/tbody/tr/td[1]/a")
-        );
-
-        Assert.assertTrue(languagesNamesList.size() > 0);
-
-        for (int i = 0; i < languagesNamesList.size(); i ++) {
-            Assert.assertTrue(languagesNamesList.get(i).getText().toLowerCase().contains(LANGUAGE_NAME));
+        for (String languageName : languageNames) {
+            Assert.assertTrue(languageName.contains(LANGUAGE_NAME));
         }
     }
 
     @Test
     public void testMainNavigationButtonGuestbook_HappyPath() {
-        final String BASE_URL = "https://www.99-bottles-of-beer.net/";
         String expectedResultGuestbookUrl = "https://www.99-bottles-of-beer.net/guestbookv2.html";
 
         getDriver().get(BASE_URL);
@@ -57,7 +93,6 @@ public class Yuliya14Test extends BaseTest {
 
     @Test
     public void testMainNavigationButtonSubmitNewLanguage_HappyPath() {
-        final String BASE_URL = "https://www.99-bottles-of-beer.net/";
         String expectedResultSubmitNewLanguageUrl = "https://www.99-bottles-of-beer.net/submitnewlanguage.html";
 
         getDriver().get(BASE_URL);
