13164f05694eda290490f43ddea25ec6bd1d8401
MariaYanu <115428442+MariaYanu@users.noreply.github.com>
2022-11-23 17:49:25 +0300

    testHeaderOnTheStartPage_HappyPath() was changed (#198)

--- src/test/java/MariaYanuTest.java
+++ src/test/java/MariaYanuTest.java
@@ -1,29 +1,44 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
 public class MariaYanuTest extends BaseTest {
-    @Test
-    public void testHeaderOnTheStartPage() {
-        final String BASE_URL = "https://www.99-bottles-of-beer.net/";
-        final String START_URL = "https://www.99-bottles-of-beer.net/";
-        final String HEADER_TEXT = "Welcome to 99 Bottles of Beer";
+    final String BASE_URL = "https://www.99-bottles-of-beer.net/";
+    final String START_URL = "https://www.99-bottles-of-beer.net/";
+    final String HEADER_TEXT = "Welcome to 99 Bottles of Beer";
+    final static By START_LINK = By.xpath("//div[@id='navigation']//a[text()='Start']");
+    final static By HEADER = By.xpath("//div[@id='main']//h2");
 
+    public void openBaseUrl(WebDriver driver) {
         getDriver().get(BASE_URL);
+    }
 
-        WebElement startLink = getDriver().findElement(
-                By.xpath("//div[@id='navigation']//a[text()='Start']")
-        );
-        startLink.click();
+    public void click(By by) {
+        getDriver().findElement(by).click();
+    }
+
+    public WebElement getElement(By by, WebDriver driver) {
+
+        return driver.findElement(by);
+    }
+
+    public String getElementText(By by, WebDriver driver) {
+
+        return driver.findElement(by).getText();
+    }
+    @Test
+    public void testHeaderOnTheStartPage_HappyPath() {
+
+        openBaseUrl(getDriver());
+        click(START_LINK);
 
         Assert.assertEquals(getDriver().getCurrentUrl(),START_URL);
 
-        WebElement header = getDriver().findElement(
-                By.xpath("//div[@id='main']//h2")
-        );
+        String currentHeader = getElementText(HEADER, getDriver());
 
-        Assert.assertEquals(header.getText(), HEADER_TEXT);
+        Assert.assertEquals(currentHeader, HEADER_TEXT);
     }
 }
