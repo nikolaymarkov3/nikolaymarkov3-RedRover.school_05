82a6fc9d5478e2cf0f94b93fa9bae50bace20852
Aram_H <63616812+AramH20@users.noreply.github.com>
2022-11-22 18:17:47 -0800

    testSearchForLanguageByName_HappyPath() refactored, locatore and methods added (#187)

--- src/test/java/AramH20Test.java
+++ src/test/java/AramH20Test.java
@@ -5,49 +5,71 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AramH20Test extends BaseTest {
+    final String BASE_URL = "https://www.99-bottles-of-beer.net/";
+    final static By SEARCH_LANGUAGES_MENU = By.xpath("//ul[@id=\"menu\"]/li/a[@href='/search.html']");
+    final static By SEARCH_FORE_FIELD = By.name("search");
+    final static By GO_BUTTON = By.name("submitsearch");
+    final static By LANGUAGES_NAMES_LIST = By.xpath("//table[@id=\"category\"]/tbody/tr/td[1]/a");
 
+    private void openBaseURL(WebDriver driver){
+        driver.get(BASE_URL);
+    }
+    private WebElement getElement(By by, WebDriver driver){
+
+        return driver.findElement(by);
+    }
+    private List<WebElement> getListOfElements(By by, WebDriver driver){
+
+        return driver.findElements(by);
+    }
+    private void click(By by, WebDriver driver){
+        getElement(by, driver).click();
+    }
+    private void input(String text, By by, WebDriver driver){
+        getElement(by, driver).sendKeys(text);
+    }
+    private int getListSize(By by, WebDriver driver){
+        return getListOfElements(by, driver).size();
+    }
+    private List<String> getElementsText(By by, WebDriver driver){
+        List<WebElement> elementsList = getListOfElements(by, driver);
+        List<String> textList = new ArrayList<>();
+
+        for (WebElement element: elementsList){
+            textList.add(element.getText().toLowerCase());
+        }
+        return textList;
+    }
     @Test
     public void testSearchForLanguageByName_HappyPath() {
-        final String BASE_URL = "https://www.99-bottles-of-beer.net/";
         final String LANGUAGE_PYTHON = "python";
 
-        getDriver().get(BASE_URL);
+        openBaseURL(getDriver());
+        click(SEARCH_LANGUAGES_MENU, getDriver());
+        click(SEARCH_FORE_FIELD, getDriver());
+        input(LANGUAGE_PYTHON, SEARCH_FORE_FIELD, getDriver());
+        click(GO_BUTTON, getDriver());
+        List<String> languageNames = getElementsText(LANGUAGES_NAMES_LIST, getDriver());
 
-        WebElement searchLanguagesMenu = getDriver().findElement(
-                By.xpath("//ul[@id=\"menu\"]/li/a[@href='/search.html']"));
-        searchLanguagesMenu.click();
-
-        WebElement searchForeField = getDriver().findElement(By.name("search"));
-        searchForeField.click();
-        searchForeField.sendKeys(LANGUAGE_PYTHON);
-
-        WebElement goButton = getDriver().findElement(By.name("submitsearch"));
-        goButton.click();
-
-        List<WebElement> languagesNamesList = getDriver().findElements(
-                By.xpath("//table[@id=\"category\"]/tbody/tr/td[1]/a"));
-
-        Assert.assertTrue(languagesNamesList.size() > 0);
-
-        for (int i = 0; i < languagesNamesList.size(); i++) {
-            Assert.assertTrue(languagesNamesList.get(i).getText().toLowerCase().contains(LANGUAGE_PYTHON));
+        Assert.assertTrue(languageNames.size() > 0);
+        for (String languageName : languageNames) {
+            Assert.assertTrue(languageName.contains(LANGUAGE_PYTHON));
         }
     }
 
 
-    final static String BASE_URL = "https://www.99-bottles-of-beer.net/";
     final static String URL_KEY_VALUE = "guest";
-    private void click(By by, WebDriver driver){
-        driver.findElement(by).click();
-    }
+    final static By MENU_GUESTBOOK = By.xpath("//ul[@id='menu']/li[5]/a");
+
     @Test
     public void test_MainFooterGuestBookv2(){
-        getDriver().get(BASE_URL);
-        By Menu_Guestbook = By.xpath("//ul[@id='menu']/li[5]/a");//
-        click(Menu_Guestbook, getDriver());
+        openBaseURL(getDriver());
+        getElement(MENU_GUESTBOOK, getDriver());
+        click(MENU_GUESTBOOK, getDriver());
         Assert.assertTrue(getDriver().getCurrentUrl().toLowerCase().contains(URL_KEY_VALUE));
     }
 }
