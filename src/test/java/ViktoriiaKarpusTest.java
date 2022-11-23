faf9bb20cdcfe4e40b3d0542c326c385d8f12eda
ViktoriiaKarpus <103855583+ViktoriiaKarpus@users.noreply.github.com>
2022-11-23 06:05:16 +0100

    [AT][[ABC][navigation][submenu][v]testNavigationSubmenuClickLetterV()<ViktoriiaKarpus> (#183)
    * Verify if user is clicking on letter V he can go on page with letter V
    
    * Update ViktoriiaKarpusTest.java

---
+++ src/test/java/ViktoriiaKarpusTest.java
@@ -0,0 +1,42 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+public class ViktoriiaKarpusTest extends BaseTest {
+
+    final String BASE_URL = "https://www.99-bottles-of-beer.net/";
+    final static By BROWSE_LANGUAGES = By.xpath("//ul[@id='menu']/li//a[@href='/abc.html']");
+
+    final static By LETTER_V = By.xpath("//div/ul[@id='submenu']/li/a[@href='v.html']");
+
+    private void openBaseURL(WebDriver driver) {
+        driver.get(BASE_URL);
+    }
+
+    private void click(By by, WebDriver driver) {
+        getElement(by, driver).click();
+    }
+
+    private WebElement getElement(By by, WebDriver driver) {
+
+        return driver.findElement(by);
+    }
+
+    @Test
+    public void testNavigationSubmenuClickLetterV() {
+        String expectedResult = "V";
+
+        openBaseURL(getDriver());
+
+        click(BROWSE_LANGUAGES, getDriver());
+        click(LETTER_V, getDriver());
+
+        String actualResult = getElement(LETTER_V, getDriver()).getText();
+
+        Assert.assertEquals(actualResult, expectedResult);
+
+    }
+}
