2b2950aee54fe6190c51658076211c435fd8b66b
Tatyana-Belikova <117529428+Tatyana-Belikova@users.noreply.github.com>
2022-11-24 00:32:59 +0200

    [RF][Submenu][Impressum] testSubmenuImpressumLabelText_Privacy<Tatyana-Belikova> (#202)
    * testSearchForLanguageFieldByName_HappyPath() refactored and methods added
    
    * transfer of locators from the testSearchForLanguageFieldByName_HappyPath()
    
    * transfer locators from the test
    
    * testSubmenuImpressumLabelText_Privacy refactored, metods addes

--- src/test/java/TatyanaBelikovaTest.java
+++ src/test/java/TatyanaBelikovaTest.java
@@ -1,51 +1,81 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TatyanaBelikovaTest extends BaseTest {
 
-    final String BASE_URL = "https://www.99-bottles-of-beer.net/";
-    String SUB_MENU_OPTION = "";
-    @Test
-    public void testSearchForLanguageFieldByName_HappyPath() {
         final String BASE_URL = "https://www.99-bottles-of-beer.net/";
         final String LANGUAGE_NAME = "python";
+        final static By SEARCH_LANGUAGES_MENU =  By.xpath("//ul[@id = 'menu']/li/a[@href = '/search.html']");
+        final static By SEARCH_FOR_FIELD = By.name("search");
+        final static By GO_BUTTON = By.name("submitsearch");
+        final static By LANGUAGES_NAMES_LIST = By.xpath("//table[@id='category']/tbody/tr/td[1]/a");
 
-        getDriver().get(BASE_URL);
+        final static By SUB_MENU_IMPRESSUM = By.xpath("//div[@id='navigation']/ul//li/a[@href='impressum.html']");
+        final String expectedResult = "Privacy";
+        private void openBaseURL(WebDriver driver) {
+            driver.get(BASE_URL);
+        }
+        private WebElement getElement(By by, WebDriver driver) {
 
-        WebElement searchLanguagesMenu = getDriver().findElement(
-                By.xpath("//ul[@id = 'menu']/li/a[@href = '/search.html']"));
-        searchLanguagesMenu.click();
+            return driver.findElement(by);
+        }
+        private String getText(By by, WebDriver driver) {
 
-        WebElement searchForField = getDriver().findElement(
-                By.name ("search"));
-        searchForField.click();
-        searchForField.sendKeys(LANGUAGE_NAME);
+            return driver.findElement(by).getText();
+        }
+        private List<WebElement> getListOfElements(By by, WebDriver driver) {
 
-        WebElement goButton = getDriver().findElement(By.name("submitsearch"));
-        goButton.click();
+            return driver.findElements(by);
+        }
+        private void click(By by, WebDriver driver) {
+            getElement(by, driver).click();
+        }
+        private void input(String text, By by, WebDriver driver) {
+            getElement(by, driver).sendKeys(text);
+        }
+        private int getListSize(By by, WebDriver driver) {
 
-        List<WebElement> languageNameList = getDriver().findElements(By.xpath("//table[@id='category']/tbody/tr/td[1]/a"));
+            return getListOfElements(by, driver).size();
+        }
+        private List<String> getElementsText(By by, WebDriver driver) {
+            List<WebElement> elementsList = getListOfElements(by, driver);
+            List<String> textList = new ArrayList<>();
 
-        Assert.assertTrue(languageNameList.size() > 0);
-        for (int i = 0; i < languageNameList.size(); i++){
-            Assert.assertTrue(languageNameList.get(i).getText().toLowerCase().contains(LANGUAGE_NAME));
+            for (WebElement element : elementsList) {
+                textList.add(element.getText().toLowerCase());
+            }
+
+            return textList;
+        }
+
+    @Test
+    public void testSearchForLanguageFieldByName_HappyPath() {
+
+            openBaseURL(getDriver());
+            click(SEARCH_LANGUAGES_MENU, getDriver());
+            click(SEARCH_FOR_FIELD, getDriver());
+            input(LANGUAGE_NAME, SEARCH_FOR_FIELD, getDriver());
+            click(GO_BUTTON, getDriver());
+
+            List<String> languageNames = getElementsText(LANGUAGES_NAMES_LIST, getDriver());
+
+            Assert.assertTrue(languageNames.size() > 0);
+
+                for (String languageName : languageNames) {
+                    Assert.assertTrue(languageName.contains(LANGUAGE_NAME));
         }
     }
     @Test
     public void testSubmenuImpressumLabelText_Privacy() {
 
-        SUB_MENU_OPTION = "impressum.html";
-        final String expectedResult = "Privacy";
+      openBaseURL(getDriver());
 
-        getDriver().get(BASE_URL);
-
-        WebElement subMenuImpressum = getDriver().findElement(
-                By.xpath("//div[@id='navigation']/ul//li/a[@href='" +SUB_MENU_OPTION+ "']"));
-
-        Assert.assertEquals(subMenuImpressum.getText(), expectedResult);
+      Assert.assertEquals(getText(SUB_MENU_IMPRESSUM, getDriver()), expectedResult);
          }
     }
