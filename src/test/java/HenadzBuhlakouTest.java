f6a4fd62c8785a86c9528b65c447944a276d34ab
Henadz Buhlakou <88853370+Papuss42@users.noreply.github.com>
2022-11-24 16:31:47 +0300

    testSearchForLanguageByName_HappyPath() refactored, locators and methods added (#214)

--- src/test/java/HenadzBuhlakouTest.java
+++ src/test/java/HenadzBuhlakouTest.java
@@ -1,53 +1,80 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
-
+import java.util.ArrayList;
 import java.util.List;
 
 public class HenadzBuhlakouTest extends BaseTest {
+    final String BASE_URL = "https://www.99-bottles-of-beer.net/";
+
+    final static By SEARCH_LANGUAGES_MENU = By.xpath("//ul[@id = 'menu']/li/a[@href = '/search.html']");
+    final static By SEARCH_FOR_FIELD = By.name("search");
+    final static By GO_BUTTON = By.name("submitsearch");
+    final static By LANGUAGES_NAMES_LIST = By.xpath("//table[@id='category']/tbody/tr/td[1]/a");
+    final static By SEARCH_START_IN_MENU = By.xpath("//div[@id='footer']//a[@href = '/']");
+    private void openBaseURL(WebDriver driver) {
+        driver.get(BASE_URL);
+    }
+
+    private WebElement getElement(By by, WebDriver driver) {
+
+        return  driver.findElement(by);
+    }
+    private List<WebElement> getListOfElements(By by, WebDriver driver) {
+
+        return driver.findElements(by);
+    }
+    private void click(By by, WebDriver driver) {
+        getElement(by, driver).click();
+    }
+
+    private void input(String text, By by, WebDriver driver) {
+        getElement(by, driver).sendKeys(text);
+    }
+    private int getListSize(By by, WebDriver driver) {
+
+        return getListOfElements(by,driver).size();
+    }
+
+    private List<String> getElementsText(By by, WebDriver driver) {
+        List<WebElement> elementsList =  getListOfElements(by, driver);
+        List<String> textList = new ArrayList<>();
+
+        for (WebElement element : elementsList) {
+            textList.add(element.getText().toLowerCase());
+        }
+
+        return textList;
+    }
 
     @Test
     public void testSearchForLanguageByName_HappyPath() {
-        final String Base_URL = "https://www.99-bottles-of-beer.net/";
         final String LANGUAGE_NAME = "python";
 
-        getDriver().get(Base_URL);
+        openBaseURL(getDriver());
+        click(SEARCH_LANGUAGES_MENU, getDriver());
+        click(SEARCH_FOR_FIELD, getDriver());
+        input(LANGUAGE_NAME, SEARCH_FOR_FIELD, getDriver());
+        click(GO_BUTTON,getDriver());
 
-        WebElement searchLanguagesMenu = getDriver().findElement(
-                By.xpath("//ul[@id = 'menu']/li/a[@href = '/search.html']")
-        );
-        searchLanguagesMenu.click();
+        List<String> languageNames = getElementsText(LANGUAGES_NAMES_LIST, getDriver());
 
-        WebElement searchForField = getDriver().findElement(By.name("search"));
-        searchForField.click();
-        searchForField.sendKeys(LANGUAGE_NAME);
+        Assert.assertTrue(languageNames.size() > 0);
 
-        WebElement goButton = getDriver().findElement(By.name("submitsearch"));
-        goButton.click();
-
-        List<WebElement> languagesNamesList = getDriver().findElements(
-                By.xpath("//table[@id='category']/tbody/tr/td[1]/a")
-        );
-
-        Assert.assertTrue(languagesNamesList.size() > 0);
-
-        for (int i = 0; i < languagesNamesList.size(); i++) {
-            Assert.assertTrue(languagesNamesList.get(i).getText().toLowerCase().contains(LANGUAGE_NAME));
+        for (String languageName : languageNames) {
+            Assert.assertTrue(languageName.contains(LANGUAGE_NAME));
         }
     }
 
     @Test
     public void testClickOnStartInFooter_HappyPath() {
-        final String BASE_URL = "https://www.99-bottles-of-beer.net/";
         String expectedResult = "https://www.99-bottles-of-beer.net/";
 
-        getDriver().get(BASE_URL);
-
-        WebElement searchStartInMenu = getDriver().findElement(
-                By.xpath("//div[@id='footer']//a[@href = '/']"));
-        searchStartInMenu.click();
+        openBaseURL(getDriver());
+        click(SEARCH_START_IN_MENU, getDriver());
 
         String actualResult = getDriver().getCurrentUrl();
 
