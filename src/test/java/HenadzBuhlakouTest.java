4a4ea7cf1e5f3a5106d37e85e38cbbe9daf127a5
Henadz Buhlakou <88853370+Papuss42@users.noreply.github.com>
2022-11-25 16:44:14 +0300

    testTextHeader2OnToplistEsoteric_HappyPath()added (#234)

--- src/test/java/HenadzBuhlakouTest.java
+++ src/test/java/HenadzBuhlakouTest.java
@@ -18,6 +18,8 @@ public class HenadzBuhlakouTest extends BaseTest {
     final static By TOP_LISTS_MENU = By.xpath("//ul[@id='menu']/li/a[@href='/toplist.html']");
     final static By TOPLIST_REAL_SUBMENU = By.xpath("//ul[@id='submenu']/li/a[@href='./toplist_real.html']");
     final static By HEADER_2_TOPLIST_REAL = By.xpath("//div[@id='main']/h2");
+    final static By TOPLIST_ESOTERIC_SUBMENU = By.xpath("//ul[@id='submenu']/li/a[@href='./toplist_esoteric.html']");
+    final static By HEADER_2_TOPLIST_ESOTERIC = By.xpath("//div[@id='main']/h2");
 
     private void openBaseURL(WebDriver driver) {
         driver.get(BASE_URL);
@@ -96,5 +98,18 @@ public class HenadzBuhlakouTest extends BaseTest {
         String actualHeader2OnToplistReal = getDriver().findElement(HEADER_2_TOPLIST_REAL).getText();
 
         Assert.assertEquals(actualHeader2OnToplistReal, expectedHeader2OnToplistReal);
+    }
+
+    @Test
+    public void testTextHeader2OnToplistEsoteric_HappyPath() {
+        final String expectedHeader2OnToplistEsoteric = "Top Rated Esoteric Languages";
+
+        openBaseURL(getDriver());
+        click(TOP_LISTS_MENU, getDriver());
+        click(TOPLIST_ESOTERIC_SUBMENU, getDriver());
+
+        String actualHeader2OnToplistEsoteric = getDriver().findElement(HEADER_2_TOPLIST_ESOTERIC).getText();
+
+        Assert.assertEquals(actualHeader2OnToplistEsoteric, expectedHeader2OnToplistEsoteric);
     }
 }
