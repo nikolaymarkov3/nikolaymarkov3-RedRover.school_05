6991228aeaaa27d1e18b52d0b14d2d17838956e9
Юлия Л <113936533+Yuliya-14@users.noreply.github.com>
2022-11-30 08:49:15 +0300

    [RF][POM][Project][Main][navigation][menu][guestbookv2] click button guestbookv2 <Yuliya14> (#280)
    * testMainNavigationButtonGuestbook_POM_CreatePage, refactored test
    
    * Update src/test/java/old_tests/Yuliya14Test.java
    
    Co-authored-by: Sergey Kuznetsov <votkinsk@gmail.com>

--- src/test/java/pages/GuestBookV2Page.java
+++ src/test/java/pages/GuestBookV2Page.java
@@ -1,10 +1,21 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class GuestBookV2Page extends GuestBookSubmenuPage {
 
+    @FindBy(xpath = "//ul[@id='menu']/li/a[@href= '/guestbookv2.html']")
+    private WebElement goButtonGuestbookV2;
+
     public GuestBookV2Page(WebDriver driver) {
         super(driver);
+    }
+
+    public GuestBookV2Page clickButtonGuestbookV2() {
+        click(goButtonGuestbookV2);
+
+        return new GuestBookV2Page(getDriver());
     }
 }
--- src/test/java/tests/GuestBookV2Test.java
+++ src/test/java/tests/GuestBookV2Test.java
@@ -1,6 +1,16 @@
 package tests;
 
 import base.BaseTest;
+import org.testng.annotations.Test;
+import pages.GuestBookV2Page;
 
 public class GuestBookV2Test extends BaseTest {
+
+    @Test
+    public void testMainNavigationButtonGuestbook() {
+
+        openBaseURL();
+        GuestBookV2Page GuestBookV2Page = new GuestBookV2Page(getDriver());
+        GuestBookV2Page.clickButtonGuestbookV2();
+    }
 }
