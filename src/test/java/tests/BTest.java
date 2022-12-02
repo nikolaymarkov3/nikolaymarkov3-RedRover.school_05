560ff6b42708c1e5803b14c5aa3e0aa595360758
Irina Z Java <66705803+ArinaJur@users.noreply.github.com>
2022-12-02 07:39:55 -0500

    Revert "[AT][Structure][Project][POM]CreateABCTestBTestMethodsWebElements<IrynaDanilevska> (#300)" (#310)
    This reverts commit 2079479a1c355e820bd2683e8826e3856aff9da9.

--- src/test/java/tests/BTest.java
+++ src/test/java/tests/BTest.java
@@ -1,51 +1,21 @@
 package tests;
 
 import base.BaseTest;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.List;
-
 public class BTest extends BaseTest {
 
-    private final String letter = "b";
-
     @Test
-    public void testSortingLanguagesByLetterB() {
-        List<String> listBLanguages =
-                openBaseURL()
-                        .clickBrowseLanguagesMenu()
-                        .clickBSubmenu()
-                        .getBLanguagesNames();
+    public void testH2Header() {
 
-        Assert.assertTrue(listBLanguages.size() > 0);
+        final String expectedHeader = "Category B";
 
-        for (String languageName : listBLanguages) {
-            Assert.assertTrue(languageName.startsWith(letter));
-        }
-    }
+        String actualHeader = openBaseURL()
+                .clickBrowseLanguagesMenu()
+                .clickBSubmenu()
+                .getH2Header();
 
-    @Test
-    public void testCatagoryWithLetterB() {
-
-        WebElement categoryBTitle =
-                openBaseURL()
-                        .clickBrowseLanguagesMenu()
-                        .clickBSubmenu()
-                        .getbCatagoryTitle();
-        Assert.assertTrue(categoryBTitle.getText().toLowerCase().endsWith(letter));
-    }
-
-    @Test
-    public void testCatagoryBDescription() {
-        String expectedDescriptionText = "All languages starting with the letter B are shown, sorted by Language.";
-
-        WebElement categoryDescriptionB =
-                openBaseURL()
-                        .clickBrowseLanguagesMenu()
-                        .clickBSubmenu()
-                        .getbCatagoryDescription();
-        Assert.assertEquals(expectedDescriptionText, categoryDescriptionB.getText());
+        Assert.assertEquals(actualHeader, expectedHeader);
     }
 }
