5947a2501dfc32f2e511b390fb71acbddd7a9a35
Anastasia Yakimova <anastasiayy@yahoo.com>
2022-12-05 02:13:43 -0500

    [RF][POM][Toplist][main][h2] testH2HeaderText() <AnastasiaYY> (#332)
    * RF testVerifyH2HeaderText() to POM

---
+++ src/test/java/tests/TopListsTest.java
@@ -0,0 +1,21 @@
+package tests;
+
+import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TopListsTest extends BaseTest {
+
+    @Test
+    public void testH2HeaderText() {
+
+        final String expectedResult = "Top Rated";
+
+        String actualResult =
+                openBaseURL()
+                        .clickTopListsMenu()
+                        .getH2HeaderText();
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+}
