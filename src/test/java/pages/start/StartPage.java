c5fb1b5068d11692ce4f840f33b65e7902d67a5c
Elena Stratulat <elenacom@inbox.ru>
2022-12-06 08:19:26 +0400

    refactored testWishesFromTheTeamFirstParagraph() (#337)

--- src/test/java/pages/start/StartPage.java
+++ src/test/java/pages/start/StartPage.java
@@ -1,10 +1,20 @@
 package pages.start;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class StartPage extends StartSubmenuPage {
 
     public StartPage(WebDriver driver) {
         super(driver);
+    }
+
+    @FindBy(xpath = "//div[@id = 'main']/p[1]")
+    private WebElement verifyText;
+
+    public String getTeamWishes () {
+
+        return getText(verifyText);
     }
 }
--- src/test/java/tests/StartTest.java
+++ src/test/java/tests/StartTest.java
@@ -3,7 +3,6 @@ package tests;
 import base.BaseTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import utils.TestUtils;
 
 import static utils.TestUtils.getBaseUrl;
 
@@ -32,4 +31,17 @@ public class StartTest extends BaseTest {
 
         Assert.assertEquals(actualUrl, expectedUrl);
     }
+
+    @Test
+    public void testWishesFromTheTeamFirstParagraph () {
+        final String WISHES_FROM_THE_TEAM = "This Website holds a collection of the Song 99 Bottles of Beer programmed in different programming languages. " +
+                "Actually the song is represented in 1500 different programming languages and variations. " +
+                "For more detailed information refer to historic information.";
+
+        String teamWishes = openBaseURL()
+                .getTeamWishes();
+
+        Assert.assertEquals(teamWishes, WISHES_FROM_THE_TEAM);
+    }
+
 }
