99ae50d9eff9167b1dad237c4a846387c5835622
verafes <verafes@gmail.com>
2022-12-08 05:21:11 -0800

    testTableListNames() on JS page added (#358)

--- src/test/java/tests/JavaScriptLanguageTest.java
+++ src/test/java/tests/JavaScriptLanguageTest.java
@@ -1,8 +1,13 @@
 package tests;
 
 import base.BaseTest;
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class JavaScriptLanguageTest extends BaseTest {
 
@@ -17,5 +22,21 @@ public class JavaScriptLanguageTest extends BaseTest {
                 .getH2HeaderText();
 
         Assert.assertEquals(actualJSH2Header, EXPECTED_JS_H2_HEADER);
+    }
+
+    @Test
+    public void testTableListNames() {
+
+        List<String> expectedListNames = new ArrayList<>(
+                Arrays.asList("Date:", "Author:", "URL:", "Comments:", "Info:", "Score:"));
+
+        List<String> actualListNames =
+                openBaseURL()
+                        .clickBrowseLanguagesMenu()
+                        .clickJSubmenu()
+                        .clickJavaScriptLink()
+                        .getTableListNames();
+
+        Assert.assertEquals(actualListNames, expectedListNames);
     }
 }
