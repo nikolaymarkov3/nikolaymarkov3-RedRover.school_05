1118383e1e2c540396548a77c53ae4b4d7ac25d2
NataliiaOliver <103964206+NataliiaOliver@users.noreply.github.com>
2022-12-08 11:37:49 -0600

    [RF][ABC][navigation][submenu][C][D] Changening the inheritance of pages <NataliiaOliver> (#362)
    * changed the intheritance of pages DelphiLanguagePage and CheetahLanguagePage

--- src/test/java/pages/browse_languages/languages/DelphiLanguagePage.java
+++ src/test/java/pages/browse_languages/languages/DelphiLanguagePage.java
@@ -1,7 +1,6 @@
 package pages.browse_languages.languages;
 
 import org.openqa.selenium.WebDriver;
-import pages.browse_languages.BrowseLanguagesSubmenuPage;
 
 public class DelphiLanguagePage extends LanguagePage {
 
