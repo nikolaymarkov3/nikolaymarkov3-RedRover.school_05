3b55909f6bc666bc3dbfa0e04f914761a3292eef
NataliiaOliver <103964206+NataliiaOliver@users.noreply.github.com>
2022-12-10 07:46:40 -0600

    Created testTextsWithLinksOnStartPage(), method, locator (#383)

--- src/test/java/pages/start/StartPage.java
+++ src/test/java/pages/start/StartPage.java
@@ -4,25 +4,35 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.List;
+
 public class StartPage extends StartSubmenuPage {
+
+    @FindBy(xpath = "//div[@id = 'main']/p[1]")
+    private WebElement verifyText;
+
+    @FindBy(xpath = "//div[@id='main']/h2")
+    private WebElement h2Header1;
+
+    @FindBy(xpath = "//div[@id='main']//a[@href]")
+    private List<WebElement> textsWithLinks;
 
     public StartPage(WebDriver driver) {
         super(driver);
     }
 
-    @FindBy(xpath = "//div[@id = 'main']/p[1]")
-    private WebElement verifyText;
-
-    public String getTeamWishes () {
+    public String getTeamWishes() {
 
         return getText(verifyText);
     }
 
-    @FindBy(xpath = "//div[@id='main']/h2")
-    private WebElement h2Header1;
-
     public String getH2() {
 
         return getText(h2Header1);
+    }
+
+    public List<String> getTextsWithLinks() {
+
+        return getListText(textsWithLinks);
     }
 }
--- src/test/java/tests/StartTest.java
+++ src/test/java/tests/StartTest.java
@@ -4,12 +4,14 @@ import base.BaseTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 import static utils.TestUtils.getBaseUrl;
 
 public class StartTest extends BaseTest {
 
     @Test
-    public void testClickStartFromHomepage(){
+    public void testClickStartFromHomepage() {
 
         String urlAfterClickingStartFromHomepage =
                 openBaseURL()
@@ -20,7 +22,7 @@ public class StartTest extends BaseTest {
     }
 
     @Test
-    public void testBaseURLShown_WhenLandingToStartPage(){
+    public void testBaseURLShown_WhenLandingToStartPage() {
         final String expectedUrl = getBaseUrl();
 
         String actualUrl =
@@ -33,7 +35,7 @@ public class StartTest extends BaseTest {
     }
 
     @Test
-    public void testWishesFromTheTeamFirstParagraph () {
+    public void testWishesFromTheTeamFirstParagraph() {
         final String WISHES_FROM_THE_TEAM = "This Website holds a collection of the Song 99 Bottles of Beer programmed in different programming languages. " +
                 "Actually the song is represented in 1500 different programming languages and variations. " +
                 "For more detailed information refer to historic information.";
@@ -45,12 +47,24 @@ public class StartTest extends BaseTest {
     }
 
     @Test
-    public void testHeaderTextH2 () {
+    public void testHeaderTextH2() {
         final String expectedHeaderH2 = "Welcome to 99 Bottles of Beer";
 
         String actualHeader = openBaseURL()
                 .getH2();
 
         Assert.assertEquals(actualHeader, expectedHeaderH2);
+    }
+
+    @Test
+    public void testTextsWithLinksOnStartPage() {
+        List<String> expectedTextsWithLinks =
+                List.of("historic information", "here", "submit your own piece of code", "guestbook", "team members");
+
+        List<String> actualTextsWithLinks =
+                openBaseURL()
+                        .getTextsWithLinks();
+
+        Assert.assertEquals(actualTextsWithLinks, expectedTextsWithLinks);
     }
 }
