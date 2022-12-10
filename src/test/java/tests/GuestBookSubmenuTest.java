6abb8558e7fb730aaecab5170ab7d9c73755eaf2
NataliiaOliver <103964206+NataliiaOliver@users.noreply.github.com>
2022-12-10 01:10:06 -0600

    [AT][Guestbookv2][navigation][submenu] testGuestBookSubmenuButtonsNames() <NataliiaOliver> (#380)
    * Created testGuestbookSubmenuButtonsNames(), method and locator

--- src/test/java/tests/GuestBookSubmenuTest.java
+++ src/test/java/tests/GuestBookSubmenuTest.java
@@ -1,6 +1,22 @@
 package tests;
 
 import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class GuestBookSubmenuTest extends BaseTest {
+
+    @Test
+    public void testGuestbookSubmenuButtonsNames() {
+        List<String> expectedGuestbookSubmenuButtonsNames = List.of("Sign Guestbook", "Read Guestbook");
+
+        List<String> actualGuestbookSubmenuButtonsNames =
+                openBaseURL()
+                        .clickGuestbookMenu()
+                        .getGuestbookSubmenuButtonsText();
+
+        Assert.assertEquals(actualGuestbookSubmenuButtonsNames, expectedGuestbookSubmenuButtonsNames);
+    }
 }
