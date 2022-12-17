e02e79bd5bf8f311dd8b98458655af206490f16f
Natallia S <happylady1806@gmail.com>
2022-12-16 23:44:00 -0500

    testKalkulonLink_NavigatesTo_KalkulonLanguagePage() added (#426)

--- src/test/java/tests/browse_languages/letters/KTest.java
+++ src/test/java/tests/browse_languages/letters/KTest.java
@@ -3,6 +3,7 @@ package tests.browse_languages.letters;
 import base.BaseTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.browse_languages.languages.KalkulonLanguagePage;
 import pages.browse_languages.languages.KotlinLanguagePage;
 import pages.browse_languages.letters.KPage;
 
@@ -44,6 +45,27 @@ public class KTest extends BaseTest {
 
         Assert.assertNotEquals(oldURL, actualURL);
         Assert.assertEquals(actualURL, expectedURL);
+        Assert.assertEquals(actualTitle, expectedTitle);
+    }
+
+    @Test
+    public void testKalkulonLink_NavigatesTo_KalkulonLanguagePage() {
+        final String expectedURL = "https://www.99-bottles-of-beer.net/language-kalkulon-740.html";
+        final String expectedTitle = "99 Bottles of Beer | Language Kalkulon";
+
+        KPage kPage = new KPage(getDriver());
+        KalkulonLanguagePage kalkulonLanguagePage = new KalkulonLanguagePage(getDriver());
+
+        String oldURL =  openBaseURL()
+                .clickBrowseLanguagesFooterMenu()
+                .clickKSubmenu()
+                .getURL();
+
+        String actualURL = kPage.clickKalkulonLanguage().getURL();
+        String actualTitle = kalkulonLanguagePage.getTitle();
+
+        Assert.assertNotEquals(oldURL, getDriver().getCurrentUrl());
+        Assert.assertEquals(actualURL,expectedURL);
         Assert.assertEquals(actualTitle, expectedTitle);
     }
 }
