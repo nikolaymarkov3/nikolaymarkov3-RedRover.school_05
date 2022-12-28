67a3855accb89510f66b75a3584eb84277e3ea28
AleksandrAkimovChg <104619642+AleksandrAkimovChg@users.noreply.github.com>
2022-12-28 06:25:46 +0300

    added priority and dependsOnMethods and deleted not so fancy tests (#443)

--- src/test/java/tests/browse_languages/BrowseLanguagesSubmenuTest.java
+++ src/test/java/tests/browse_languages/BrowseLanguagesSubmenuTest.java
@@ -2,51 +2,16 @@ package tests.browse_languages;
 
 import TestData.TestData;
 import base.BaseTest;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.browse_languages.BrowseLanguagesSubmenuPage;
-import pages.browse_languages.letters.ABCPage;
-import pages.browse_languages.letters.NPage;
 
 import java.util.List;
 
 public class BrowseLanguagesSubmenuTest extends BaseTest {
 
-    @Test
-    public void testASubmenuLink() {
-        final String expectedLink = "https://www.99-bottles-of-beer.net/a.html";
-
-        String actualLink = openBaseURL()
-                .clickBrowseLanguagesMenu()
-                .clickASubmenu()
-                .getURL();
-
-        Assert.assertEquals(actualLink, expectedLink);
-    }
-
-    @Test
-    public void testNSymbolTextAndLink() {
-        final String expectedSymbol = "N";
-        final String expectedLink = "https://www.99-bottles-of-beer.net/n.html";
-
-        NPage nPage = new NPage(getDriver());
-
-        String oldURL = openBaseURL()
-                .clickBrowseLanguagesMenu()
-                .getURL();
-
-        new ABCPage(getDriver()).clickNSubmenu();
-
-        Assert.assertNotEquals(oldURL, nPage.getURL());
-
-        String actualSymbol = nPage.getNSubmenuText();
-        String actualLink = nPage.getNHref();
-
-        Assert.assertEquals(actualSymbol, expectedSymbol);
-        Assert.assertEquals(actualLink, expectedLink);
-    }
-
-    @Test
+    @Test(priority = -4)
     public void testTextAndLinksSubmenu() {
         final List<String> expectedlettersSubmenu = List.of("0-9", "A", "B", "C", "D", "E", "F", "G", "H", "I"
                 , "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
@@ -59,22 +24,23 @@ public class BrowseLanguagesSubmenuTest extends BaseTest {
         Assert.assertEquals(actualSubmenusNames, expectedlettersSubmenu);
     }
 
-    @Test(dataProvider = "lettersSubmenu", dataProviderClass = TestData.class)
+    @Test(priority = -4, dependsOnMethods = "testTextAndLinksSubmenu",
+            dataProvider = "lettersSubmenu", dataProviderClass = TestData.class)
     public void testLetterSubmenuNavigate(
             int index, String symbol, String url, String title) {
 
-        ABCPage abcPage = openBaseURL()
+        BrowseLanguagesSubmenuPage browseLanguagesSubmenuPage = openBaseURL()
                 .clickBrowseLanguagesMenu();
 
-        String oldURL = abcPage.getURL();
-        String oldTitle = abcPage.getTitle();
-        String actualSymbol = abcPage.getTextSymbol(index);
-        String actualURLHref = abcPage.getHref(index);
+        List<WebElement> lettersList = browseLanguagesSubmenuPage.getSubmenus();
 
-        abcPage.clickOnSymbolSubmenu(index);
+        String oldURL = browseLanguagesSubmenuPage.getURL();
+        String oldTitle = browseLanguagesSubmenuPage.getTitle();
+        String actualSymbol =  browseLanguagesSubmenuPage.getTextSymbol(index);
+        String actualURLHref = browseLanguagesSubmenuPage.getHref(index);
 
-        String actualURL = getDriver().getCurrentUrl();
-        String actualTitle = getDriver().getTitle();
+        String actualURL = browseLanguagesSubmenuPage.clickMenu(index, lettersList).getURL();
+        String actualTitle = browseLanguagesSubmenuPage.clickMenu(index, lettersList).getTitle();
 
         if (index != 1) {
             Assert.assertNotEquals(actualURL, oldURL);
