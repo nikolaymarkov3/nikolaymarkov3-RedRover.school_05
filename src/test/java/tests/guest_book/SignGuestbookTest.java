b0a6b5b9e9cb23b6fffe68deb2e19b73ee3e4b2d
Natallia S <happylady1806@gmail.com>
2023-01-20 23:45:10 -0500

    conflicts resolved

--- src/test/java/tests/guest_book/SignGuestbookTest.java
+++ src/test/java/tests/guest_book/SignGuestbookTest.java
@@ -382,13 +382,6 @@ public class SignGuestbookTest extends BaseTest {
 
 
     @Test
-    public void testTextInMessageField_WhenClickSubmitButton() {
-        final String urlLink = "URL link";
-        final String webSiteTitle = "Web site title";
-        final String expectedMessageText = "[url=URL link]Web site title[/url]";
-
-    
-    @Test
     public void testUrlIcon() {
         final String expectedUrlIcon = "https://www.99-bottles-of-beer.net/images/bb/bburl.gif";
 
@@ -401,7 +394,7 @@ public class SignGuestbookTest extends BaseTest {
     }
     
     @Test
-    public void testPromptAlertsTexts_WhenClickUrlIcon () {
+    public void testPromptAlertsTexts_WhenClickUrlIcon() {
         final String expectedFirstPromptAlert =
                 "Enter the URL for the link you want to add.";
         final String expectedSecondPromptAlert =
@@ -414,14 +407,6 @@ public class SignGuestbookTest extends BaseTest {
                 .clickUrlIcon();
 
         SignGuestbookPage signGuestbookPage = new SignGuestbookPage(getDriver());
-
-        signGuestbookPage.acceptTwoAlerts(urlLink, webSiteTitle);
-
-        String actualMessageText = signGuestbookPage
-                .clickSubmitButton()
-                .getMessageText();
-
-        Assert.assertEquals(actualMessageText, expectedMessageText);
         
         String actualFirstPromptAlert = signGuestbookPage.getAlertText();
 
@@ -431,5 +416,26 @@ public class SignGuestbookTest extends BaseTest {
 
         Assert.assertEquals(actualFirstPromptAlert, expectedFirstPromptAlert);
         Assert.assertEquals(actualSecondPromptAlert, expectedSecondPromptAlert);
+    }
+
+    @Test
+    public void testTextInMessageField_WhenClickSubmitButton() {
+        final String urlLink = "URL link";
+        final String webSiteTitle = "Web site title";
+        final String expectedMessageText = "[url=URL link]Web site title[/url]";
+
+        openBaseURL()
+                .clickGuestbookMenu()
+                .clickSignGuestbookSubmenu()
+                .clickUrlIcon();
+        SignGuestbookPage signGuestbookPage = new SignGuestbookPage(getDriver());
+
+        signGuestbookPage.acceptTwoAlerts(urlLink, webSiteTitle);
+
+        String actualMessageText = signGuestbookPage
+                .clickSubmitButton()
+                .getMessageText();
+
+        Assert.assertEquals(actualMessageText, expectedMessageText);
     }
 }
